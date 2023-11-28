a36a066c98f16df697cba8087d3758a20d6e05bb
Shane Mindreau <smindre1@binghamton.edu>
2023-11-27 20:55:26 -0500

    I finished the 'User' schema model.

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill in a valid email address'],
+    },
+    thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    },
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length()
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
