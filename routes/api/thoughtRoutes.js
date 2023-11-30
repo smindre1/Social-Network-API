14913c733f60b35c6a750cc947226591284fee18
Shane Mindreau <smindre1@binghamton.edu>
2023-11-29 22:44:04 -0500

    I finished the user and thought route files located in the api folder.

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+const {getThoughts, getSpecificThought, newThought, updateThought, deleteThought, newReaction, deleteReaction} = require('../../controllers/thoughtControllers');
 
+router.route('/').get(getThoughts).post(newThought);
 
+router.route('/:thoughtId').get(getSpecificThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(newReaction).delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,10 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { getUsers, getSpecificUser, newUser, updateUser, deleteUser, newFriend, deleteFriend } = require("../../controllers/userControllers");
 
+router.route("/").get(getUsers).post(newUser);
 
+router.route("/:userId").get(getSpecificUser).put(updateUser).delete(deleteUser);
 
-
+router.route("/:userId/friends/:friendId").post(newFriend).delete(deleteFriend);
 
 module.exports = router;
